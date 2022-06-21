036dcb36744bd31618030821174c23238eae7f01
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-06-21 11:25:49 +0100

    Edited feedback table code

--- db/scripts/populateFeedTable.js
+++ db/scripts/populateFeedTable.js
@@ -1,11 +1,11 @@
-// import {resources} from "../../libs/resources.js";
+import {feedback} from "../../libs/feedback.js";
 import { pool } from "../index.js";
 
 async function populateFeedTable() {
   for (let i = 0; i < feedback.length; i++) {
     const res = await pool.query(
-      `INSERT INTO feedback (title, description, category, week, link, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
-      [feedback[i].bootcamper_id, feedback[i].name, feedback[i].coach, feedback[i].scores]
+      `INSERT INTO feedback (name, coach, scores) VALUES ($1, $2, $3) RETURNING *;`,
+      [feedback[i].name, feedback[i].coach, feedback[i].scores]
     );
     console.log(res.rows[0], "inserted.");
   }
