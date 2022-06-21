cc8b144276e352cb718ff9153b4e3a8d1509fe04
cragglesummers <73234010+AlphaPentagon@users.noreply.github.com>
2022-06-21 10:21:51 +0100

    Commited remaining files

---
+++ libs/resources.js
@@ -0,0 +1,19 @@
+export const resources = [
+    {
+        title: "Title1",
+        description: "Desc1",
+        category: "Cat1",
+        week: 1,
+        link: "link1",
+        image: "image1"
+    },    
+    {
+        title: "Title2",
+        description: "Desc2",
+        category: "Cat2",
+        week: 2,
+        link: "link2",
+        image: "image2"
+    }, 
+
+]
